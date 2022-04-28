f300f6415f8d58394ac5e6f4ad0dd100a99c5830
scordoni <54602744+scordoni@users.noreply.github.com>
2022-04-27 23:15:14 -0400

    Got basic code gen working for basic program

--- src/CodeGeneration.java
+++ src/CodeGeneration.java
@@ -1,3 +1,4 @@
+import java.lang.reflect.Executable;
 
 /*
 * 
@@ -17,9 +18,19 @@ public class CodeGeneration {
 
     static int i;
 
+    static int n;
+
     static int t;
 
-    static int j = 0;
+    static int temp;
+
+    static int temp1;
+
+    static int j;
+
+    static int k;
+
+    static int l;
 
     static int currentScope = -1;
 
@@ -29,7 +40,7 @@ public class CodeGeneration {
 
     static String[] executableImage = new String[256];
 
-    static String[][] staticData = new String[10][10];
+    static String[][] staticData = new String[10][3];
 
     static String[][] jumps = new String[10][10];
 
@@ -43,6 +54,9 @@ public class CodeGeneration {
         ErrorFlag = 0;
         i = 0;
         t = 0;
+        j = 0;
+        k = 0;
+        l = 0;
 
         currentAstNode = root;
 
@@ -55,8 +69,46 @@ public class CodeGeneration {
         //traverse through the AST for code generation
         traverse(currentAstNode, i);
 
-        //if we have error then output error message and no table or output symbol table
+        //back patching? i forget what we call it
 
+        System.out.println("temp1 " + temp1);
+
+        System.out.println("16 i " + Integer.toString(temp1, 16).toUpperCase());
+
+        
+
+
+        //for each null value we replace it with 00
+        for(int i = 0; i < executableImage.length; i++){
+
+            if(executableImage[i] == null){
+                executableImage[i] = "00";
+            }//if
+
+        }//for
+
+        for(int r = 0; r < executableImage.length; r++){
+
+
+            if((executableImage[r].compareToIgnoreCase("T0") == 0) && (executableImage[r+1].compareToIgnoreCase("XX") == 0)){
+
+                executableImage[r] = Integer.toString(temp1, 16).toUpperCase();
+
+                executableImage[r+1] = "00";
+
+            }//if
+
+            if((executableImage[r].compareToIgnoreCase("T1") == 0) && (executableImage[r+1].compareToIgnoreCase("XX") == 0)){
+                
+                executableImage[r] = Integer.toString(temp1, 16).toUpperCase() + 1;
+
+                executableImage[r+1] = "00";
+
+            }//if
+
+        }//for
+
+        //if we have error then output error message or executable image
         if(ErrorFlag == 1){
 
             System.out.println(" ");
@@ -75,6 +127,7 @@ public class CodeGeneration {
             for(int i = 0; i < executableImage.length; i++){
 
                 System.out.print(executableImage[i]);
+                System.out.print(" ");
 
             }//for
 
@@ -89,14 +142,23 @@ public class CodeGeneration {
 
     //Recursive function to handle the traversal through the tree
     //similar to expand but not printing anything
-    public static void traverse(ASTNode currentAstNode, int i){
+    public static int traverse(ASTNode currentAstNode, int i){
 
-        //System.out.println(currentAstNode.getName());
-
+        
+        System.out.println(" ");
+        System.out.println(currentAstNode.getName());
+        System.out.println("Top i " + i);
+        System.out.println(" ");
+        
+        
         //look into length of each instruction and add to th array from there
 
-        //if we have block stmt then we need a new table and to up the scope
+        //if we have then we skip
         if(currentAstNode.getName().compareToIgnoreCase("Block") == 0){
+
+        }//if
+
+        if(currentAstNode.getName().compareToIgnoreCase("ID") == 0){
 
 
         }//if
@@ -106,7 +168,8 @@ public class CodeGeneration {
         if(currentAstNode.getName().compareToIgnoreCase("VarDecl") == 0){
 
             if(currentAstNode.children.get(0).getSymbol().compareToIgnoreCase("int") == 0 ){
-                
+
+                //add to executable image
                 //A9 00 8D T# XX
                 executableImage[i] = "A9";
                 i++;
@@ -117,10 +180,26 @@ public class CodeGeneration {
                 executableImage[i] = "T"+t;
                 i++;
                 executableImage[i] = "XX";
+                i++;
+
+                //add to static image
+                //T# XX var name and address
+
+                staticData[j][k] = "T" + t + "XX";
+                k++;
+                staticData[j][k] = currentAstNode.children.get(1).getSymbol();
+                k++;
+                staticData[j][k] = "0";
+
+                j++;
+                k = 0;
+
             
             }//if
             
             else if (currentAstNode.children.get(0).getSymbol().compareToIgnoreCase("string") == 0){
+
+                
 
 
             }//else if
@@ -145,7 +224,7 @@ public class CodeGeneration {
                 //A9 # 8D T# XX
                 executableImage[i] = "A9";
                 i++;
-                executableImage[i] = "0" + currentAstNode.children.get(1).toString();
+                executableImage[i] = "0" + currentAstNode.children.get(1).getSymbol().toString();
                 i++;
                 executableImage[i] = "8D";
                 i++;
@@ -153,6 +232,7 @@ public class CodeGeneration {
                 i++;
                 t++;
                 executableImage[i] = "XX";
+                i++;
             
             }//if
 
@@ -163,7 +243,7 @@ public class CodeGeneration {
                 //A9 # 8D T# XX
                 executableImage[i] = "A9";
                 i++;
-                executableImage[i] = "0" + currentAstNode.children.get(1).toString();
+                executableImage[i] = "0" + currentAstNode.children.get(1).getSymbol().toString();
                 i++;
                 executableImage[i] = "8D";
                 i++;
@@ -171,6 +251,7 @@ public class CodeGeneration {
                 i++;
                 t++;
                 executableImage[i] = "XX";
+                i++;
             
             }//else if
 
@@ -202,6 +283,7 @@ public class CodeGeneration {
             i++;
             t++;
             executableImage[i] = "01";
+            i++;
 
         }//if
 
@@ -221,15 +303,25 @@ public class CodeGeneration {
 
         //else we traverse through the ast to the next node
         else{
+
+            temp = i;
+
+            temp1 = temp + i;
                 
             //traverse through the AST
             for (int m = 0; m < currentAstNode.children.size(); m++){
 
-                traverse(currentAstNode.children.get(m), i);
-                
+                traverse(currentAstNode.children.get(m), temp);
+
             }//for
 
         }//else
+
+        System.out.println("After temp " + temp);
+        System.out.println("After i " + i);
+        System.out.println("After temp and i " + temp1);
+
+        return temp1;
        
     }//traverse
     
